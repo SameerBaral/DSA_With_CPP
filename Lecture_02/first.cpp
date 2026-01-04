89bca5e597c15f8e5dc0f6348deb8143f4c2152b
Sameer Baral <baralsameer70@gmail.com>
2026-01-04 23:12:43 +0530

    conditional and switch case

--- Lecture_02/first.cpp
+++ Lecture_02/first.cpp
@@ -87,15 +87,48 @@ int main(){
 
     // Increment and Decrement operator
     // pre-increment and post-increment
-    int a = 10;
+    // int a = 10;
     // int b = a++;   //post-increment
     // post-increment pehle value assign karta hai fir increment karta hai
 
-    int b = ++a;   //pre-increment
+    // int b = ++a;   //pre-increment
     // pre-increment pehle increment karta hai fir value assign karta hai
     
-    cout<<b<<endl;   
-    cout<<a<<endl;   
+    // cout<<b<<endl;   
+    // cout<<a<<endl;   
+
+
+    // --------------------
+
+    // int a1 = 10;
+
+    // int b1 = a1--;   //post-decrement
+    // post-decrement pehle value assign karta hai fir decrement karta hai
+
+    // int b1 = --a1;   //pre-decrement
+    // pre-decrement pehle decrement karta hai fir value assign karta hai
+
+    // cout<<b1<<endl;   
+    // cout<<a1<<endl;
+
+
+    // -------------------------------------
+
+    // bitwise AND , OR , XOR
+
+    // int a = 5;      // 0101
+    // int b = 10;      // 1010
+
+    // cout<<(a & b)<<endl;   // 0000 -> 0
+    // cout<<(a | b)<<endl;   // 1111 -> 15
+    // cout<<(0 ^ 0)<<endl;   // 0
+    // cout<<(0 ^ 1)<<endl;   // 1
+    // cout<<(1 ^ 1)<<endl;   // 0
+
+    // XOR : if both bits are same then it will return 0 otherwise 1
+
+
+
     return 0;
 
 
---
+++ Lecture_02/second.cpp
@@ -0,0 +1,138 @@
+
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    // int age;
+
+    // cout<<"Enter your age: ";
+    // cin>>age;
+
+    // if(age>=18){
+    //     cout<<"You are eligible for vote"<<endl;
+    // }
+    // else{
+    //     cout<<"You are not eligible for vote"<<endl;
+    // }
+
+
+    // --------------------------------------
+
+
+    // int marks;
+    // cout<<"Enter your marks: ";
+    // cin>>marks;
+
+    // if(marks>=33){
+    //     cout<<"You are pass"<<endl;
+    // }
+    // else{
+    //     cout<<"You are fail"<<endl;
+    // }
+
+
+
+    // A+ : marks > 90
+    // A : marks > 80 && marks <=90
+    // B+ : marks > 70 && marks <=80
+    // B : marks > 60 && marks <=70
+    // C : 
+
+
+    // if(marks>90){
+    //     cout<<"Grade: A+"<<endl;
+    // }
+    // else if(marks>80){
+    //     cout<<"Grade: A"<<endl;
+    // }
+    // else if(marks>70){
+    //     cout<<"Grade: B+"<<endl;
+    // }
+    // else if(marks>60){
+    //     cout<<"Grade: B"<<endl;
+    // }
+    // else{
+    //     cout<<"Grade: C"<<endl;
+    // }
+
+
+
+
+    // --------------------------------------
+
+    // Switch case:-
+
+    // int day;
+    // cout<<"Enter day number (1-7): ";
+    // cin>>day;
+    // switch(day){
+    //     case 1:
+    //         cout<<"Monday"<<endl;
+    //         break;
+    //     case 2:
+    //         cout<<"Tuesday"<<endl;
+    //         break;
+    //     case 3:
+    //         cout<<"Wednesday"<<endl;
+    //         break;
+    //     case 4:
+    //         cout<<"Thursday"<<endl;
+    //         break;
+    //     case 5:
+    //         cout<<"Friday"<<endl;
+    //         break;
+    //     case 6:
+    //         cout<<"Saturday"<<endl;
+    //         break;
+    //     case 7:
+    //         cout<<"Sunday"<<endl;
+    //         break;
+    //     default:
+    //         cout<<"Invalid day number"<<endl;
+    // }
+
+
+
+    // Given number is even or odd
+    // int number;
+    // cout<<"Enter a number: ";
+    // cin>>number;
+
+    // if(number%2==0){
+    //     cout<<"Even number"<<endl;
+    // }
+    // else{
+    //     cout<<"Odd number"<<endl;
+    // }
+
+
+
+    // Find largest among two numbers
+
+    // int a,b;
+    // cout<<"Enter first number: ";
+    // cin>>a;
+    // cout<<"Enter second number: ";
+    // cin>>b;
+    // if(a>b){
+    //     cout<<a<<" is largest"<<endl;
+    // }
+    // else{
+    //     cout<<b<<" is largest"<<endl;
+    // }
+
+
+    // ========================================
+
+    // loops=> repeatition of a task again and again
+
+    // for loop
+
+    for(int i=1;i<=5;i++){
+        cout<<"Hello World"<<endl;
+    }
+
+
+    return 0;
+}
